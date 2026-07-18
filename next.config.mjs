dcf44437708fe21db58651881be81be3505dee3b
rananomi05 <noman987rajpoot@gmail.com>
2026-07-18 18:06:17 +0500

    Fix ESLint build errors

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+  },
+};
 
 export default nextConfig;
